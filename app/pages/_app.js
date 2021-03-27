c5d68ec350db5c106f0f5a6a5990d7cd1ceb1899
takaya787 <takaya318318@gmail.com>
2021-03-27 12:26:54 +0900

    indexページ bootstrap導入

--- app/pages/_app.js
+++ app/pages/_app.js
@@ -1,4 +1,6 @@
 import '../styles/globals.css'
+// Importing the Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,65 +1,42 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.scss'
+import Link from 'next/link'
+import Image from 'next/image'
+//components
+import { Layout } from '../components/Layout'
+//Bootstrap
+import { Nav } from 'react-bootstrap'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
+    <>
+      <Layout>
+        <Head>
+          <title>Create Next App</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <body>
+          <header className="navbar navbar-fixed-top navbar-inverse">
+            <div className="container">
+              <Nav>
+                <ul className="nav navbar-nav navbar-right">
+                  <li><Link href="/"><a>Home</a></Link></li>
+                  <li><Link href="/help"><a>Help</a></Link></li>
+                  <li><Link href="/login"><a>Login</a></Link></li>
+                </ul>
+              </Nav>
+            </div>
+          </header>
+          <div className="center jumbotron">
+            <h1>Welcome to the Sample App</h1>
+            <h2>This is the home page for the
+              <a href="https://railstutorial.jp/">Ruby on Rails Tutorial</a>sample application.
+            </h2>
+            <Link href="/signup"><button className="btn btn-lg btn-primary">Sign up!</button></Link>
+          </div>
+          <Image src="/images/rails.svg" alt="log of rails" width={500} height={300} />
+        </body>
+      </Layout>
+    </>
   )
 }

49ed99769bb0be72bedbaa3187c201b1d15dfe01
takaya787 <takaya318318@gmail.com>
2021-04-20 18:05:01 +0900

    Authmoduleとuser context作成

--- app/pages/_app.js
+++ app/pages/_app.js
@@ -1,9 +1,24 @@
 import '../styles/globals.css'
 // Importing the Bootstrap CSS
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState, useEffect, createContext } from 'react';
+//Module
+import { Auth } from '../modules/Auth'
+
+export const UserContext = createContext();
+const baseUrl = `${process.env.NEXT_PUBLIC_BASE_URL}auto_login`
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [user, setUser] = useState({ email: '', id: 0, name: '' })
+  const UserValue = {
+    user,
+    setUser,
+  };
+  return (
+    <UserContext.Provider value={UserValue}>
+      <Component {...pageProps} />
+    </UserContext.Provider>
+  )
 }
 
 export default MyApp

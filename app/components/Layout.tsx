661ae11344975ff35d1adcf9648a0cb6cae8655c
takaya787 <takaya318318@gmail.com>
2021-08-11 14:07:27 +0900

    基本のcomponentをprettierで整形

--- app/components/Layout.tsx
+++ app/components/Layout.tsx
@@ -1,162 +1,176 @@
-import React, { useCallback, useEffect, useState, useMemo } from 'react'
-import Head from 'next/head';
-import { useRecoilState } from 'recoil';
-import styles from './Layout.module.scss';
+import React, { useCallback, useEffect, useState, useMemo } from "react";
+import Head from "next/head";
+import { useRecoilState } from "recoil";
+import styles from "./Layout.module.scss";
 //Atoms
-import { FlashMessageAtom } from '../Atoms/FlashMessageAtom';
+import { FlashMessageAtom } from "../Atoms/FlashMessageAtom";
 
 //Bootstrap
-import Alert from 'react-bootstrap/Alert'
-import Button from 'react-bootstrap/Button'
+import Alert from "react-bootstrap/Alert";
+import Button from "react-bootstrap/Button";
 //components
-import { Header } from './Header'
-import { Footer } from './Footer'
+import { Header } from "./Header";
+import { Footer } from "./Footer";
 //context
 // import { FlashMessageContext } from '../pages/_app'
 //Moudle
-import { Auth } from '../modules/Auth'
+import { Auth } from "../modules/Auth";
 //hooks
-import { useUserSWR } from '../hooks/useUserSWR'
-import { useFlashReducer } from '../hooks/useFlashReducer';
+import { useUserSWR } from "../hooks/useUserSWR";
+import { useFlashReducer } from "../hooks/useFlashReducer";
 
 export const Layout: React.FC<{
-  children: React.ReactNode
-}> = ({
-  children
-}) => {
-    // userdataをSWRから取り出す
-    const { user_data, has_user_key } = useUserSWR()
+  children: React.ReactNode;
+}> = ({ children }) => {
+  // userdataをSWRから取り出す
+  const { user_data, has_user_key } = useUserSWR();
 
-    //Flash Message Atomを読み込み
-    const [FlashAtom, setFlashAtom] = useRecoilState(FlashMessageAtom)
-    //useFlashReducerを読み込み
-    const { FlashReducer } = useFlashReducer()
+  //Flash Message Atomを読み込み
+  const [FlashAtom, setFlashAtom] = useRecoilState(FlashMessageAtom);
+  //useFlashReducerを読み込み
+  const { FlashReducer } = useFlashReducer();
 
-    //Scrollでのy座標を状態として所持する
-    const [scrollY, setScrollY] = useState(0)
+  //Scrollでのy座標を状態として所持する
+  const [scrollY, setScrollY] = useState(0);
 
-    const FlashClose = useCallback(() => {
-      FlashReducer({ type: "HIDDEN", message: "" })
-    }, [])
+  const FlashClose = useCallback(() => {
+    FlashReducer({ type: "HIDDEN", message: "" });
+  }, []);
 
-    const scrollTop = (): number => { // scroll位置を取る関数。
-      return Math.max(              // なんかブラウザによってとり方が違うようなので全部もってきてMaxをとる
-        window.pageYOffset,
-        document.documentElement.scrollTop,
-        document.body.scrollTop);
-    };
+  const scrollTop = (): number => {
+    // scroll位置を取る関数。
+    return Math.max(
+      // なんかブラウザによってとり方が違うようなので全部もってきてMaxをとる
+      window.pageYOffset,
+      document.documentElement.scrollTop,
+      document.body.scrollTop
+    );
+  };
 
-    const scrollHandler = (): void => {
-      const position = scrollTop();
-      setScrollY(position)
-    }
+  const scrollHandler = (): void => {
+    const position = scrollTop();
+    setScrollY(position);
+  };
 
-    const Resend_Email_Url = process.env.NEXT_PUBLIC_BASE_URL + 'account_activations/resend_email'
+  const Resend_Email_Url = process.env.NEXT_PUBLIC_BASE_URL + "account_activations/resend_email";
 
-    //resend_requestを作成するClick関数
-    const click_resend_email = () => {
-      fetch(Resend_Email_Url, {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${Auth.getToken()}`
-        }
-      })
-        .then((response) => response.json())
-        .then((data) => {
-          console.log({ data })
-          FlashReducer({ type: "SUCCESS", message: data.message })
-        })
-    }
+  //resend_requestを作成するClick関数
+  const click_resend_email = () => {
+    fetch(Resend_Email_Url, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${Auth.getToken()}`,
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log({ data });
+        FlashReducer({ type: "SUCCESS", message: data.message });
+      });
+  };
 
-    //React.Element関連
-    //scrollによる条件分けを含んだAlertの構文
-    const Alert_Block = useMemo((): React.ReactElement => {
-      return (
-        <>
-          {
-            scrollY >= 130 ? (
-              <div className="fixed-top m-3">
-                <Alert show={FlashAtom.show} variant={FlashAtom.variant} transition={true} bsPrefix="alert">
-                  <div className={`${styles.flash_message}`}>
-                    {FlashAtom.message}
-                  </div>
-                </Alert>
-              </div>) : (
-              <div className="shadow">
-                <Alert show={FlashAtom.show} variant={FlashAtom.variant} transition={true} bsPrefix="alert">
-                  <div className={`${styles.flash_message}`}>
-                    {FlashAtom.message}
-                  </div>
-                </Alert>
-              </div>
-            )
-          }
-        </>
-      )
-    }, [FlashClose, FlashAtom, scrollY])
+  //React.Element関連
+  //scrollによる条件分けを含んだAlertの構文
+  const Alert_Block = useMemo((): React.ReactElement => {
+    return (
+      <>
+        {scrollY >= 130 ? (
+          <div className="fixed-top m-3">
+            <Alert
+              show={FlashAtom.show}
+              variant={FlashAtom.variant}
+              transition={true}
+              bsPrefix="alert"
+            >
+              <div className={`${styles.flash_message}`}>{FlashAtom.message}</div>
+            </Alert>
+          </div>
+        ) : (
+          <div className="shadow">
+            <Alert
+              show={FlashAtom.show}
+              variant={FlashAtom.variant}
+              transition={true}
+              bsPrefix="alert"
+            >
+              <div className={`${styles.flash_message}`}>{FlashAtom.message}</div>
+            </Alert>
+          </div>
+        )}
+      </>
+    );
+  }, [FlashClose, FlashAtom, scrollY]);
 
-    const Activation_Warning = useMemo((): React.ReactElement => {
-      return (
-        <>
-          <Alert variant="warning">
-            <Alert.Heading>Your account is not still activated!</Alert.Heading>
-            <p className="font-weight-bold text-danger">Please confirm your email and activate your Account by clicking the Link in the email</p>
-          </Alert>
-          <Alert variant="info">
-            <Alert.Heading>Resend the activation email</Alert.Heading>
-            <p className="font-weight-bold text-info">If you can't confirm the activation email in your account, the email can be resent by here</p>
-            <Button variant="outline-primary" onClick={click_resend_email}>Resend</Button>
-          </Alert>
-        </>
-      )
-    }, [])
+  const Activation_Warning = useMemo((): React.ReactElement => {
+    return (
+      <>
+        <Alert variant="warning">
+          <Alert.Heading>Your account is not still activated!</Alert.Heading>
+          <p className="font-weight-bold text-danger">
+            Please confirm your email and activate your Account by clicking the Link in the email
+          </p>
+        </Alert>
+        <Alert variant="info">
+          <Alert.Heading>Resend the activation email</Alert.Heading>
+          <p className="font-weight-bold text-info">
+            If you can't confirm the activation email in your account, the email can be resent by
+            here
+          </p>
+          <Button variant="outline-primary" onClick={click_resend_email}>
+            Resend
+          </Button>
+        </Alert>
+      </>
+    );
+  }, []);
 
-    //use Effect関連
-    //flash messageを5秒後に消す
-    useEffect(function () {
+  //use Effect関連
+  //flash messageを5秒後に消す
+  useEffect(
+    function () {
       if (FlashAtom.show) {
         setTimeout(FlashClose, 5000);
       }
-    }, [FlashAtom.show])
+    },
+    [FlashAtom.show]
+  );
 
-    //scrollのeventListenerを設置しておく
-    useEffect(function () {
-      document.addEventListener("scroll", scrollHandler);
-      return (): void => document.removeEventListener("scroll", scrollHandler);
-    })
+  //scrollのeventListenerを設置しておく
+  useEffect(function () {
+    document.addEventListener("scroll", scrollHandler);
+    return (): void => document.removeEventListener("scroll", scrollHandler);
+  });
 
+  return (
+    <div className={styles.container}>
+      <Head>
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <meta charSet="UTF-8" />
+        <title>Ruby on Rails tutorial</title>
 
+        {/* favicon関連 */}
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
+        <link rel="manifest" href="/favicons/site.webmanifest" />
+        <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#000000" />
+        <link rel="shortcut icon" href="/favicons/favicon.ico" />
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta name="msapplication-config" content="/favicons/browserconfig.xml" />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
 
-    return (
-      <div className={styles.container}>
-        <Head>
-          <meta name="viewport" content="width=device-width,initial-scale=1" />
-          <meta charSet="UTF-8" />
-          <title>Ruby on Rails tutorial</title>
+      <Header />
+      {Alert_Block}
+      {/* activataされていないユーザーには通知する */}
+      {Auth.isLoggedIn() && user_data && has_user_key() && !user_data.user.activated && (
+        <> {Activation_Warning}</>
+      )}
 
-          {/* favicon関連 */}
-          <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
-          <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
-          <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
-          <link rel="manifest" href="/favicons/site.webmanifest" />
-          <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#000000" />
-          <link rel="shortcut icon" href="/favicons/favicon.ico" />
-          <meta name="msapplication-TileColor" content="#ffffff" />
-          <meta name="msapplication-config" content="/favicons/browserconfig.xml" />
-          <meta name="theme-color" content="#ffffff" />
-        </Head>
+      <main className={styles.main}>{children}</main>
 
-        <Header />
-        {Alert_Block}
-        {/* activataされていないユーザーには通知する */}
-        {Auth.isLoggedIn() && user_data && has_user_key() && !user_data.user.activated &&
-          (<> {Activation_Warning}</>)
-        }
-
-        <main className={styles.main}>{children}</main>
-
-        <Footer />
-      </div>
-    )
-  }
+      <Footer />
+    </div>
+  );
+};
--- app/components/Modal/Modal.tsx
+++ app/components/Modal/Modal.tsx
@@ -1,13 +1,13 @@
 import React, { useState } from "react";
-import styles from './Modal.module.scss';
+import styles from "./Modal.module.scss";
 //components
-import { UserForm } from './UserForm';
-import { LoginForm } from './LoginForm';
-import { PasswordResetForm } from './PasswordResetForm'
+import { UserForm } from "./UserForm";
+import { LoginForm } from "./LoginForm";
+import { PasswordResetForm } from "./PasswordResetForm";
 
 type ModalPropsType = {
-  title: string
-}
+  title: string;
+};
 
 export const Modal: React.FC<ModalPropsType> = ({ title }) => {
   const [isSignup, setIsSignup] = useState<boolean>(false);
@@ -20,45 +20,51 @@ export const Modal: React.FC<ModalPropsType> = ({ title }) => {
     setIsLogin(false);
     setIsPasswordReset(false);
     setIsOpen(false);
-  }
+  };
   const Signupcontroll = (): void => {
     setIsSignup(!isSignup);
     setIsLogin(false);
     setIsPasswordReset(false);
     setIsOpen(true);
-  }
+  };
   const Logincontroll = (): void => {
     setIsSignup(false);
     setIsPasswordReset(false);
     setIsLogin(!isLogin);
     setIsOpen(true);
-  }
+  };
 
   const PasswordResetcontroll = (): void => {
     setIsSignup(false);
     setIsPasswordReset(!isPasswordReset);
     setIsLogin(false);
     setIsOpen(true);
-  }
+  };
 
   return (
     <>
       {/* 始めはボタンが表示される */}
-      { !isOpen &&
-        (<button onClick={Signupcontroll} className={styles.initial}>{title}</button>)
-      }
+      {!isOpen && (
+        <button onClick={Signupcontroll} className={styles.initial}>
+          {title}
+        </button>
+      )}
 
       {/* Sign up用のformが表示される */}
-      { isOpen &&
-        (<div className={styles.modal}>
+      {isOpen && (
+        <div className={styles.modal}>
           <div className={styles.content}>
-            <button className={styles.content_close} onClick={Closemodal}>×</button>
+            <button className={styles.content_close} onClick={Closemodal}>
+              ×
+            </button>
             {/* signup用のformが表示される */}
             {isSignup && (
               <>
                 <h2 className={styles.content_title}>Sign up！</h2>
                 <UserForm Closemodal={Closemodal} />
-                <button className={styles.content_switch} onClick={Logincontroll}>Login is Here!</button>
+                <button className={styles.content_switch} onClick={Logincontroll}>
+                  Login is Here!
+                </button>
               </>
             )}
             {/* Login用のformが表示される */}
@@ -66,22 +72,28 @@ export const Modal: React.FC<ModalPropsType> = ({ title }) => {
               <>
                 <h2 className={styles.content_title}>Log in！</h2>
                 <LoginForm Closemodal={Closemodal} PasswordResetControll={PasswordResetcontroll} />
-                <button className={styles.content_switch} onClick={Signupcontroll}>Sign up is Here!</button>
+                <button className={styles.content_switch} onClick={Signupcontroll}>
+                  Sign up is Here!
+                </button>
               </>
             )}
             {isPasswordReset && (
               <>
                 <h2 className={`${styles.content_title} text-danger my-0`}>Password Reset!</h2>
-                <p className={"text-danger text-left my-2 ml-4"}>The Password Reset Link is sent to <br />your emaill adress after confirming your address.</p>
+                <p className={"text-danger text-left my-2 ml-4"}>
+                  The Password Reset Link is sent to <br />
+                  your emaill adress after confirming your address.
+                </p>
 
                 <PasswordResetForm Closemodal={Closemodal} />
-                <button className={styles.content_switch} onClick={Logincontroll}>Back to Login Form!</button>
+                <button className={styles.content_switch} onClick={Logincontroll}>
+                  Back to Login Form!
+                </button>
               </>
             )}
           </div>
         </div>
-        )
-      }
+      )}
     </>
-  )
-}
+  );
+};

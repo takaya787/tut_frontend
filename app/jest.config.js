048ab782aafd044a67eca413b328187fb6744d41
takaya787 <takaya318318@gmail.com>
2021-12-28 12:19:14 +0900

    feat: next,reactを最新にした

--- app/jest.config.js
+++ app/jest.config.js
@@ -1,14 +1,14 @@
 module.exports = {
-  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
-  roots: ['<rootDir>'],
-  testEnvironment: 'jsdom',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'json', 'jsx'],
-  testPathIgnorePatterns: ['<rootDir>[/\\\\](node_modules|.next)[/\\\\]'],
-  transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(ts|tsx)$'],
+  setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"],
+  roots: ["<rootDir>"],
+  testEnvironment: "jsdom",
+  moduleFileExtensions: ["ts", "tsx", "js", "json", "jsx"],
+  testPathIgnorePatterns: ["<rootDir>[/\\\\](node_modules|.next)[/\\\\]"],
+  transformIgnorePatterns: ["[/\\\\]node_modules[/\\\\].+\\.(ts|tsx)$"],
   transform: {
-    '^.+\\.(ts|tsx)$': 'babel-jest',
+    "^.+\\.(js|jsx|ts|tsx)$": ["babel-jest", { presets: ["next/babel"] }],
   },
   moduleNameMapper: {
-    '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
+    "\\.(css|less|sass|scss)$": "identity-obj-proxy",
   },
 };
